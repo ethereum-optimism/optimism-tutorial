e1f61d0d8c359b612cd25f161bfd57ecfe86abcc
Jan Klosinski <vanjan@protonmail.com>
2021-03-27 12:37:57 +0100

    Attempt OVM compilation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,8 @@ import { HardhatUserConfig } from 'hardhat/types'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-waffle'
 
+import '@eth-optimism/plugins/hardhat/compiler'
+
 const config: HardhatUserConfig = {
   solidity: "0.7.3",
 };
