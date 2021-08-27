31c608829ded33448a909a8c43a461c3332ea780
Ori Pomerantz <ori@optimism.io>
2021-08-27 19:43:20 +0000

    Several updates:
    1. Added the `eth_getLogs` 10k block limitation to README.md
    2. Added a warning about this issue to traceTx.sh
    3. Changed infuraID to come from stdin, to reduce the chance of such a value leaking

--- core-util-watcher/traceTx.sh
+++ core-util-watcher/traceTx.sh
@@ -1,18 +1,23 @@
 #! /bin/sh
 
-infuraID= your value here
+echo Infura ID?
+read infuraID
+
+
+echo The hashes in this script are old, and may be beyond the 10k block limit. Look in
+echo Etherscan to find newer transactions
 
 # mainnet L1 -> L2 message
-./index.js -l 1 -n mainnet -i $infuraID --hash 0x122abee5d2f41da16a40e7edd0c5460d8f50683a46e1765fee2a8a8eb8b01cc2
+node index.js -l 1 -n mainnet -i $infuraID --hash 0x122abee5d2f41da16a40e7edd0c5460d8f50683a46e1765fee2a8a8eb8b01cc2
 
 echo ------------------------
 # mainnet L2 -> L1 message
-./index.js -l 2 -n mainnet -i $infuraID --hash 0x0c3357cb993d26b5aa54a883bbd1918b08a3054f68cb0b69af7971e0ae49f9f1
+node index.js -l 2 -n mainnet -i $infuraID --hash 0x0c3357cb993d26b5aa54a883bbd1918b08a3054f68cb0b69af7971e0ae49f9f1
 
 echo ----------------------
 # kovan L1 -> L2
-./index.js -n kovan -i $infuraID --hash 0x65d50ed9d1edd7f20e97229667b5fc892fb23b128c8baca960c47c74fde9d4ed
+node index.js -n kovan -i $infuraID --hash 0x65d50ed9d1edd7f20e97229667b5fc892fb23b128c8baca960c47c74fde9d4ed
 
 echo -----------------------
 # kovan L2 -> L1
-./index.js -l 2 -n kovan -i $infuraID --hash 0xa018b12df969c0f65aec3c3d08c94332525a309cdec38206eef74319c1d1dd30
+node index.js -l 2 -n kovan -i $infuraID --hash 0xa018b12df969c0f65aec3c3d08c94332525a309cdec38206eef74319c1d1dd30
