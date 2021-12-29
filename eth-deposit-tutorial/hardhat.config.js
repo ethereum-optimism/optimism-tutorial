6f18086703d638667dc2d3925bbaa2775021aac6
Ori Pomerantz <qbzzt1@gmail.com>
2021-12-29 18:20:55 +0200

    Initial version

---
+++ eth-deposit-tutorial/hardhat.config.js
@@ -0,0 +1,20 @@
+// Plugins
+require('@nomiclabs/hardhat-ethers')
+
+module.exports = {
+  networks: {
+    hardhat: {
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk'
+      }
+    },
+    optimism: {
+      url: 'http://127.0.0.1:8545',
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk'
+      },
+      gasPrice: 0
+    }
+  },
+  solidity: '0.7.6'
+}
