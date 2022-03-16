7415bcb39d02844a6c68fdb892831bfcbf67c695
Ori Pomerantz <qbzzt1@gmail.com>
2022-03-16 14:12:06 -0500

    feat(trace-tx): getSymbol

--- sdk-trace-tx/index.js
+++ sdk-trace-tx/index.js
@@ -23,16 +23,40 @@ const setup = async() => {
 }    // setup
 
 
+// Only the part of the ABI we need to get the symbol
+const ERC20ABI = [
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+        {
+            "name": "",
+            "type": "string"
+        }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  }
+]     // ERC20ABI
 
+
+
+const getSymbol = async l1Addr => {
+  if (l1Addr == '0x0000000000000000000000000000000000000000')
+    return "ETH"
+  const l1Contract = new ethers.Contract(l1Addr, ERC20ABI, crossChainMessenger.l1SignerOrProvider)
+  return await l1Contract.symbol()  
+}   // getSymbol
 
 // Describe a cross domain transaction, either deposit or withdrawal
 const describeTx = async tx => {
   console.log(`tx:${tx.transactionHash}`)
   // Assume all tokens have decimals = 18
-  console.log(`\tAmount: ${tx.amount/1e18} of ${tx.l1Token}`)
+  console.log(`\tAmount: ${tx.amount/1e18} ${await getSymbol(tx.l1Token)}`)
   console.log(`\tRelayed: ${await crossChainMessenger.getMessageStatus(tx.transactionHash)  
                               == optimismSDK.MessageStatus.RELAYED}`)
-  console.log(`\n`)
 }  // describeTx
 
 
@@ -45,12 +69,12 @@ const main = async () => {
     const deposits = await crossChainMessenger.getDepositsByAddress(addr)
     console.log(`Deposits by address ${addr}`)
     for (var i=0; i<deposits.length; i++)
-      describeTx(deposits[i])
+      await describeTx(deposits[i])
 
     const withdrawals = await crossChainMessenger.getWithdrawalsByAddress(addr)
     console.log(`\n\n\nWithdrawals by address ${addr}`)
     for (var i=0; i<withdrawals.length; i++)
-      describeTx(withdrawals[i])
+      await describeTx(withdrawals[i])
 
     // Some extra fields compared to the get<verb>ByAddress results
 //    const l1toL2msg = (await crossChainMessenger.getMessagesByTransaction(depositHash))[0]
