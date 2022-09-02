27be6b9f635ab1714a8befc5990e8f93b7c8a297
Ori Pomerantz <qbzzt1@gmail.com>
2022-09-02 18:51:29 -0500

    feat(xdom-bridge): Remove erc20 code from eth tutorial

--- cross-dom-bridge/index.js
+++ cross-dom-bridge/index.js
@@ -12,18 +12,8 @@ const l1Url = `https://eth-goerli.g.alchemy.com/v2/${process.env.GOERLI_ALCHEMY_
 const l2Url = `https://opt-goerli.g.alchemy.com/v2/${process.env.OPTIMISM_GOERLI_ALCHEMY_KEY}`
 
 
-// Contract addresses for DAI tokens, taken
-// from https://static.optimism.io/optimism.tokenlist.json
-const daiAddrs = {
-  l1Addr: "0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa",
-  l2Addr: "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1"
-}    // daiAddrs
-
-
-
 // Global variable because we need them almost everywhere
 let crossChainMessenger
-let l1ERC20, l2ERC20    // DAI contracts to show ERC-20 transfers
 let addr    // Our address
 
 const getSigners = async () => {
@@ -47,29 +37,13 @@ const setup = async() => {
       l1SignerOrProvider: l1Signer,
       l2SignerOrProvider: l2Signer
   })
-//  l1ERC20 = new ethers.Contract(daiAddrs.l1Addr, erc20ABI, l1Signer)
-//  l2ERC20 = new ethers.Contract(daiAddrs.l2Addr, erc20ABI, l2Signer)
 }    // setup
 
-
-
-// The ABI fragment for an ERC20 we need to get a user's balance.
-const erc20ABI = [
-    // balanceOf
-    {
-      constant: true,
-      inputs: [{ name: "_owner", type: "address" }],
-      name: "balanceOf",
-      outputs: [{ name: "balance", type: "uint256" }],
-      type: "function",
-    },
-  ]    // erc20ABI
 
 
 const gwei = 1000000000n
 const eth = gwei * gwei   // 10^18
 const centieth = eth/100n
-const dai = eth
 
 
 const reportBalances = async () => {
@@ -78,17 +52,6 @@ const reportBalances = async () => {
 
   console.log(`On L1:${l1Balance} Gwei    On L2:${l2Balance} Gwei`)
 }    // reportBalances
-
-
-
-const reportERC20Balances = async () => {
-  const l1Balance = (await l1ERC20.balanceOf(addr)).toString().slice(0,-18)
-  const l2Balance = (await l2ERC20.balanceOf(addr)).toString().slice(0,-18)
-  console.log(`DAI on L1:${l1Balance}     DAI on L2:${l2Balance}`)
-}    // reportERC20Balances
-
-
-
 
 
 const depositETH = async () => {
@@ -110,64 +73,6 @@ const depositETH = async () => {
 }     // depositETH()
 
 
-
-const depositERC20 = async () => {
-
-  console.log("Deposit ERC20")
-  await reportERC20Balances()
-  const start = new Date()
-
-  // Need the l2 address to know which bridge is responsible
-  const allowanceResponse = await crossChainMessenger.approveERC20(
-    daiAddrs.l1Addr, daiAddrs.l2Addr, dai)
-  await allowanceResponse.wait()
-  console.log(`Allowance given by tx ${allowanceResponse.hash}`)
-  console.log(`Time so far ${(new Date()-start)/1000} seconds`)
-
-  const response = await crossChainMessenger.depositERC20(
-    daiAddrs.l1Addr, daiAddrs.l2Addr, dai)
-  console.log(`Deposit transaction hash (on L1): ${response.hash}`)
-  await response.wait()
-  console.log("Waiting for status to change to RELAYED")
-  console.log(`Time so far ${(new Date()-start)/1000} seconds`)
-  await crossChainMessenger.waitForMessageStatus(response.hash,
-                                                  optimismSDK.MessageStatus.RELAYED)
-
-  await reportERC20Balances()
-  console.log(`depositERC20 took ${(new Date()-start)/1000} seconds\n\n`)
-}     // depositETH()
-
-
-
-const withdrawERC20 = async () => {
-
-  console.log("Withdraw ERC20")
-  const start = new Date()
-  await reportERC20Balances()
-
-  const response = await crossChainMessenger.withdrawERC20(
-    daiAddrs.l1Addr, daiAddrs.l2Addr, dai)
-  console.log(`Transaction hash (on L2): ${response.hash}`)
-  await response.wait()
-
-  console.log("Waiting for status to change to IN_CHALLENGE_PERIOD")
-  console.log(`Time so far ${(new Date()-start)/1000} seconds`)
-  await crossChainMessenger.waitForMessageStatus(response.hash, 
-    optimismSDK.MessageStatus.IN_CHALLENGE_PERIOD)
-  console.log("In the challenge period, waiting for status READY_FOR_RELAY") 
-  console.log(`Time so far ${(new Date()-start)/1000} seconds`)  
-  await crossChainMessenger.waitForMessageStatus(response.hash, 
-                                                optimismSDK.MessageStatus.READY_FOR_RELAY) 
-  console.log("Ready for relay, finalizing message now")
-  console.log(`Time so far ${(new Date()-start)/1000} seconds`)  
-  await crossChainMessenger.finalizeMessage(response)
-  console.log("Waiting for status to change to RELAYED")
-  console.log(`Time so far ${(new Date()-start)/1000} seconds`)  
-  await crossChainMessenger.waitForMessageStatus(response, 
-    optimismSDK.MessageStatus.RELAYED)
-  await reportERC20Balances()   
-  console.log(`withdrawERC20 took ${(new Date()-start)/1000} seconds\n\n\n`)  
-}     // withdrawERC20()
 
 
 
