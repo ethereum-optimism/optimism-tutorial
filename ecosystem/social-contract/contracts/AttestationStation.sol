39f0f053aa4a5d268545212dd4e7b423615581ff
Zain Bacchus <zainbacchus@gmail.com>
2022-11-03 17:11:47 -0700

    Update and rename SocialContract.sol to AttestationStation.sol

--- ecosystem/social-contract/contracts/AttestationStation.sol
+++ ecosystem/social-contract/contracts/AttestationStation.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-contract SocialContract {
+contract AttestationStation {
     mapping(address => mapping(address => mapping(bytes32 => bytes))) public attestations;
 
     struct AttestationData {
